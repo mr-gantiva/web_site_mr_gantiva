cc5460edc62987c12f01473c5b0fdd6c02da4cfb
mr-gantiva <ivaganva@gmail.com>
2023-06-06 13:20:50 -0400

    Agregando libreria ScrollReveal y animaciones a navbar y titulos

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.container-nav', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    sr.reveal('.titulo-container', {
+        duration: 3000,
+        origin: 'right',
+        distance: '400px',
+    });
